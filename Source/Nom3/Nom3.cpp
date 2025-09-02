7c6ea6fb2c549a147bdc1879c9f64b95cde9334e
허지웅 <joe0617160@naver.com>
2025-09-02 17:11:44 +0900

    First Commit

---
+++ Source/Nom3/Nom3.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nom3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nom3, "Nom3" );
+
+//
+DEFINE_LOG_CATEGORY(NOM3);
