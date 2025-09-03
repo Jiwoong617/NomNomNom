861534c8f31fc362ada5c55a3f3b1f6f1b2d91a5
허지웅 <joe0617160@naver.com>
2025-09-03 21:47:22 +0900

    Bind Keys and FPS Movement

--- Source/Nom3/Private/Core/NomPlayer.cpp
+++ Source/Nom3/Private/Core/NomPlayer.cpp
@@ -3,19 +3,82 @@
 
 #include "Core/NomPlayer.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Nom3/Nom3.h"
+
 // Sets default values
 ANomPlayer::ANomPlayer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//TPS Mesh
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Quinn_Simple.SKM_Quinn_Simple'"));
+	if (TempMesh.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(TempMesh.Object);
+		GetMesh()->SetRelativeLocation(FVector(0, 0, -87));
+		GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
+
+		GetMesh()->SetOwnerNoSee(true);
+	}
+
+	//FSP Mesh
+	FpsMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPS Mesh"));
+	FpsMeshComp->SetupAttachment(GetRootComponent());
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempFpsMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny_Simple.SKM_Manny_Simple'"));
+	if (TempFpsMesh.Succeeded())
+	{
+		FpsMeshComp->SetSkeletalMesh(TempFpsMesh.Object);
+		FpsMeshComp->SetRelativeLocation(FVector(0, 0, -87));
+		FpsMeshComp->SetRelativeRotation(FRotator(0, -90, 0));
+		FpsMeshComp->FirstPersonPrimitiveType = EFirstPersonPrimitiveType::FirstPerson;
+
+		//내 카메라에만 보이게
+		FpsMeshComp->SetOnlyOwnerSee(true);
+	}
+	
+	//FPS Cam Settings
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("FPS Spring Arm");
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->SetRelativeLocation(FVector(320.000000,0.000000,80.000000));
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("FPS Cam");
+	CameraComp->SetupAttachment(SpringArmComp);
+	CameraComp->bUsePawnControlRotation = true;
+	CameraComp->bEnableFirstPersonFieldOfView = true;
+	CameraComp->bEnableFirstPersonScale = true;
+	CameraComp->FirstPersonFieldOfView = 70.0f;
+	CameraComp->FirstPersonScale = 0.6f;
+
+	//Basic Init
+	JumpMaxCount = 2;
+	GetCharacterMovement()->NavAgentProps.bCanCrouch = true;
+	GetCharacterMovement()->MaxWalkSpeed = MaxSpeed;
+	GetCharacterMovement()->JumpZVelocity = JumpForce;
+	GetCharacterMovement()->GravityScale *= GravityMultiplier;
+	GetCharacterMovement()->AirControl = 0.5f;
 }
 
 // Called when the game starts or when spawned
 void ANomPlayer::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	auto pc = Cast<APlayerController>(GetController());
+	if (pc)
+	{
+		auto subsys = ULocalPlayer::GetSubsystem
+		<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+		if (subsys)
+		{
+			subsys->AddMappingContext(IMC_NomPlayer, 0);
+		}
+	}
+
 }
 
 // Called every frame
@@ -29,6 +92,136 @@ void ANomPlayer::Tick(float DeltaTime)
 void ANomPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(IA_Move, ETriggerEvent::Triggered, this, &ANomPlayer::MoveInput);
+		EnhancedInputComponent->BindAction(IA_Look, ETriggerEvent::Triggered, this, &ANomPlayer::LookInput);
+		EnhancedInputComponent->BindAction(IA_Jump, ETriggerEvent::Started, this, &ANomPlayer::JumpInput);
+		EnhancedInputComponent->BindAction(IA_Run, ETriggerEvent::Started, this, &ANomPlayer::RunToggle);
+		EnhancedInputComponent->BindAction(IA_Crouch, ETriggerEvent::Started, this, &ANomPlayer::CrouchToggle);
 
+		EnhancedInputComponent->BindAction(IA_Interact, ETriggerEvent::Triggered, this, &ANomPlayer::InteractHold);
+
+		EnhancedInputComponent->BindAction(IA_Fire, ETriggerEvent::Triggered, this, &ANomPlayer::Fire);
+		EnhancedInputComponent->BindAction(IA_Aim, ETriggerEvent::Triggered, this, &ANomPlayer::Aim);
+		EnhancedInputComponent->BindAction(IA_Melee, ETriggerEvent::Started, this, &ANomPlayer::Melee);
+		EnhancedInputComponent->BindAction(IA_Throw, ETriggerEvent::Started, this, &ANomPlayer::Throw);
+		EnhancedInputComponent->BindAction(IA_Skill, ETriggerEvent::Started, this, &ANomPlayer::Skill);
+		EnhancedInputComponent->BindAction(IA_Ultimate, ETriggerEvent::Started, this, &ANomPlayer::UltimateSkill);
+	}
+}
+
+void ANomPlayer::MoveInput(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	MoveFunc(MovementVector.X, MovementVector.Y);
+}
+
+void ANomPlayer::LookInput(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+	LookFunc(LookAxisVector.X, LookAxisVector.Y);
+}
+
+void ANomPlayer::JumpInput()
+{
+	Jump();
+}
+
+void ANomPlayer::RunToggle()
+{
+	bIsRunning = !bIsRunning;
+	GetCharacterMovement()->MaxWalkSpeed = bIsRunning ? MaxSpeed * 2 : MaxSpeed;
+}
+
+void ANomPlayer::CrouchToggle()
+{
+	CrouchOrSlide();
+	bIsCrouching = !bIsCrouching;
+}
+
+void ANomPlayer::LookFunc(float Yaw, float Pitch)
+{
+	if (GetController())
+	{
+		// pass the rotation inputs
+		AddControllerYawInput(Yaw);
+		AddControllerPitchInput(Pitch);
+	}
+}
+
+void ANomPlayer::MoveFunc(float Right, float Forward)
+{
+	if (GetController())
+	{
+		// pass the move inputs
+		AddMovementInput(GetActorRightVector(), Right);
+		AddMovementInput(GetActorForwardVector(), Forward);
+	}
+}
+
+void ANomPlayer::CrouchOrSlide()
+{
+	if (bIsRunning)
+	{
+		//슬라이드
+		PRINTINFO();
+	}
+	else
+	{
+		if (bIsCrouching)
+			UnCrouch();
+		else
+			Crouch();
+	}
+}
+
+void ANomPlayer::InteractHold(const FInputActionValue& Value)
+{
+	float HoldDuration = Value.Get<float>();
+	if (true)
+	{
+		InteractDuration = HoldDuration;
+		// TODO : 에이밍 중이면
+		if (HoldDuration >= 1.f)
+		{
+			PRINTINFO();
+			InteractDuration = 0.f;
+		}
+	}
+	else
+	{
+		InteractDuration = 0.f;
+	}
+}
+
+void ANomPlayer::Fire()
+{
+	PRINTINFO();
+}
+
+void ANomPlayer::Aim()
+{
+	PRINTINFO();
+}
+
+void ANomPlayer::Melee()
+{
+	PRINTINFO();
+}
+
+void ANomPlayer::Throw()
+{
+	PRINTINFO();
+}
+
+void ANomPlayer::Skill()
+{
+	PRINTINFO();
+}
+
+void ANomPlayer::UltimateSkill()
+{
+	PRINTINFO();
 }
 
--- Source/Nom3/Public/Core/NomPlayer.h
+++ Source/Nom3/Public/Core/NomPlayer.h
@@ -6,6 +6,13 @@
 #include "GameFramework/Character.h"
 #include "NomPlayer.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+struct FInputActionValue;
+class UInputMappingContext;
+class UInputAction;
+
+
 UCLASS()
 class NOM3_API ANomPlayer : public ACharacter
 {
@@ -25,5 +32,102 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	//Basic Status
+	float MaxSpeed = 800.f;
+	float JumpForce = 600.f;
+	float GravityMultiplier = 0.75f;
 
+	//Movement params
+	bool bIsRunning = false;
+	bool bIsCrouching = false;
+	bool bIsAiming = false;
+	float InteractDuration = 0.f;
+	
+	
+	//Fps Mesh
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* FpsMeshComp;
+	
+	//Camera
+	UPROPERTY(EditAnywhere)
+	USpringArmComponent* SpringArmComp;
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* CameraComp;
+
+	//Inputs
+	UPROPERTY(EditAnywhere)
+	UInputMappingContext* IMC_NomPlayer;
+
+	//Movement
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Move;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Jump;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Look;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Run;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Crouch;
+	
+	// Interaction
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Interact;
+
+	// Combat
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Fire;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Aim;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Melee;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Throw;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Skill;
+	UPROPERTY(EditAnywhere)
+	UInputAction* IA_Ultimate;
+
+public:
+
+protected:
+	//Movement InputEvent
+	UFUNCTION()
+	void MoveInput(const FInputActionValue& Value);
+	UFUNCTION()
+	void LookInput(const FInputActionValue& Value);
+	UFUNCTION()
+	void JumpInput();
+	UFUNCTION()
+	void RunToggle();
+	UFUNCTION()
+	void CrouchToggle();
+	
+	//MoveMent Func
+	UFUNCTION()
+	virtual void LookFunc(float Yaw, float Pitch);
+	UFUNCTION()
+	virtual void MoveFunc(float Right, float Forward);
+	UFUNCTION()
+	void CrouchOrSlide();
+
+	//interaction
+	UFUNCTION()
+	void InteractHold(const FInputActionValue& Value);
+
+	//Battle
+	UFUNCTION()
+	void Fire();
+	UFUNCTION()
+	void Aim();
+	UFUNCTION()
+	void Melee();
+	UFUNCTION()
+	void Throw();
+	UFUNCTION()
+	void Skill();
+	UFUNCTION()
+	void UltimateSkill();
 };
