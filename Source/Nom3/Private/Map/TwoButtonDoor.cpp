c7c545141b1f517dbd14c5b3b68d26a3faf00e5d
jammim0209 <woals1375@naver.com>
2025-09-19 02:27:45 +0900

    [MAP] celanmachin 및 twobutton door 기믹 제작 완료

--- Source/Nom3/Private/Map/TwoButtonDoor.cpp
+++ Source/Nom3/Private/Map/TwoButtonDoor.cpp
@@ -1,113 +1,51 @@
- // Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
-  #include "Map/TwoButtonDoor.h"
-  #include "Map/InteractiveButton.h"
+#include "Map/TwoButtonDoor.h"
+#include "Map/InteractiveButton.h"
+#include "Nom3/Nom3.h"
 
-  ATwoButtonDoor::ATwoButtonDoor()
-  {
-      PrimaryActorTick.bCanEverTick = true;
+ATwoButtonDoor::ATwoButtonDoor()
+{
+    // 더 이상 Tick 함수를 사용하지 않으므로 비활성화합니다.
+    PrimaryActorTick.bCanEverTick = false;
 
-      bShouldBeOpen = false;
-      bIsButton1Active = false;
-      bIsButton2Active = false;
-  }
+    bIsButton1Active = false;
+    bIsButton2Active = false;
+}
 
-  void ATwoButtonDoor::BeginPlay()
-  {
-      Super::BeginPlay();
+void ATwoButtonDoor::BeginPlay()
+{
+    Super::BeginPlay();
+}
 
-      // 시작 위치와 목표 위치 설정 (AutoDoor 로직과 동일)
-      StartLocation = GetActorLocation();
-      GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+void ATwoButtonDoor::ActivateButton(AInteractiveButton* Button)
+{
+    if (Button == Button1)
+    {
+        bIsButton1Active = true;
+        PRINTLOG(TEXT("Button 1 활성화"));
+    }
+    else if (Button == Button2)
+    {
+        bIsButton2Active = true;
+        PRINTLOG(TEXT("Button 2 활성화"));
+    }
+    CheckDoorState();
+}
 
-      if (StartLocation.IsNearlyZero())
-      {
-          StartLocation = GetActorLocation();
-      }
-      if (GlobalTargetLocation.IsNearlyZero())
-      {
-          GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
-      }
+void ATwoButtonDoor::DeactivateButton(AInteractiveButton* Button)
+{
+    // 현재 로직에서는 버튼이 비활성화되지 않습니다.
+}
 
-      MaxSpeed = ObjectSpeed;
-      ObjectSpeed = 0.0f;
-  }
-
-  void ATwoButtonDoor::Tick(float DeltaTime)
-  {
-      Super::Tick(DeltaTime);
-
-      //auto door 랑 비슷하게
-      FVector Destination = bShouldBeOpen ? GlobalTargetLocation : StartLocation;
-      float DistanceToDestination = FVector::Dist(GetActorLocation(), Destination);
-
-      float TargetSpeed = (DistanceToDestination > 1.0f) ? MaxSpeed : 0.0f;
-
-      ObjectSpeed = FMath::FInterpTo(ObjectSpeed, TargetSpeed, DeltaTime, Acceleration);
-
-      if (FMath::IsNearlyZero(ObjectSpeed) && FMath::IsNearlyZero(TargetSpeed))
-      {
-          if (DistanceToDestination > 0.0f)
-          {
-              SetActorLocation(Destination);
-          }
-          ObjectSpeed = 0.0f;
-          return;
-      }
-
-      FVector Direction = (Destination - GetActorLocation()).GetSafeNormal();
-      FVector Movement = Direction * ObjectSpeed * DeltaTime;
-
-      if (Movement.Size() >= DistanceToDestination)
-      {
-          SetActorLocation(Destination);
-          ObjectSpeed = 0.0f;
-      }
-      else
-      {
-          SetActorLocation(GetActorLocation() + Movement);
-      }
-  }
-
-  void ATwoButtonDoor::ActivateButton(AInteractiveButton* Button)
-  {
-      if (Button == Button1)
-      {
-          bIsButton1Active = true;
-          UE_LOG(LogTemp, Warning, TEXT("Button 1 활성화"));
-      }
-      else if (Button == Button2)
-      {
-          bIsButton2Active = true;
-          UE_LOG(LogTemp, Warning, TEXT("Button 2 활성화"));
-      }
-      CheckDoorState();
-  }
-
-  void ATwoButtonDoor::DeactivateButton(AInteractiveButton* Button)
-  {
-      if (Button == Button1)
-      {
-          bIsButton1Active = false;
-          UE_LOG(LogTemp, Warning, TEXT("Button 1 비 활성화"));
-      }
-      else if (Button == Button2)
-      {
-          bIsButton2Active = false;
-          UE_LOG(LogTemp, Warning, TEXT("Button 2 비 활성화"));
-      }
-      CheckDoorState();
-  }
-    // 여기서 결정
-  void ATwoButtonDoor::CheckDoorState()
-  {
-      if (bIsButton1Active && bIsButton2Active)
-      {
-          bShouldBeOpen = true;
-      }
-      else
-      {
-          bShouldBeOpen = false;
-      }
-  }
-  
+void ATwoButtonDoor::CheckDoorState()
+{
+    if (bIsButton1Active && bIsButton2Active)
+    {
+       PRINTLOG(TEXT("두 버튼 활성화: 문이 사라집니다."));
+        
+        // 액터를 숨기고 충돌을 비활성화합니다.
+        SetActorHiddenInGame(true);
+        SetActorEnableCollision(false);
+    }
+}
--- Source/Nom3/Public/Map/InteractiveButton.h
+++ Source/Nom3/Public/Map/InteractiveButton.h
@@ -16,7 +16,7 @@ class NOM3_API AInteractiveButton : public AActor
 public:
 	AInteractiveButton();
 
-protected:
+public:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
@@ -29,6 +29,4 @@ protected:
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool		bFromSweep, const FHitResult& SweepResult);
 
-	UFUNCTION()
-	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
