87b9b3cf66c0b2ac4604bfb958bbab6ebf88ed4e
jammim0209 <woals1375@naver.com>
2025-09-21 15:37:43 +0900

    [MAP] 최적화및 포스트 프로세싱 완

--- Source/Nom3/Private/Map/TwoButtonDoor.cpp
+++ Source/Nom3/Private/Map/TwoButtonDoor.cpp
@@ -6,7 +6,6 @@
 
 ATwoButtonDoor::ATwoButtonDoor()
 {
-    // 더 이상 Tick 함수를 사용하지 않으므로 비활성화합니다.
     PrimaryActorTick.bCanEverTick = false;
 
     bIsButton1Active = false;
@@ -18,6 +17,7 @@ void ATwoButtonDoor::BeginPlay()
     Super::BeginPlay();
 }
 
+// 버튼이 눌렸다면
 void ATwoButtonDoor::ActivateButton(AInteractiveButton* Button)
 {
     if (Button == Button1)
@@ -35,16 +35,18 @@ void ATwoButtonDoor::ActivateButton(AInteractiveButton* Button)
 
 void ATwoButtonDoor::DeactivateButton(AInteractiveButton* Button)
 {
-    // 현재 로직에서는 버튼이 비활성화되지 않습니다.
+    // 비활성화 따윈 없다
 }
 
+
+//
 void ATwoButtonDoor::CheckDoorState()
 {
     if (bIsButton1Active && bIsButton2Active)
     {
        PRINTLOG(TEXT("두 버튼 활성화: 문이 사라집니다."));
         
-        // 액터를 숨기고 충돌을 비활성화합니다.
+        // 비활성화 처리
         SetActorHiddenInGame(true);
         SetActorEnableCollision(false);
     }
